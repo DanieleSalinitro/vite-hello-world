2ee5e7d3ff97971e79da7aa5e7ebc0428186f9bf
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-04-10 08:55:03 +0200

    esercizio base ultimato

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-
-  </div>
+  <AppTitle />
 </template>
 
 <script>
+  import AppTitle from './components/AppTitle.vue'
   export default {
-    
+    name: 'App',
+    data() {
+      return {
+        
+      }
+    }, 
+    methods: {
+      
+    }, 
+    mounted() {
+      
+    },
+    components: {
+      AppTitle
+    }
   }
 </script>
 
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,32 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',  
+        data() {
+            return {
+                title: 'La mia prima app con Vite!'
+            }
+        },
+        methods: {
+        
+        }, 
+        mounted() {
+            
+        }, 
+        components: {
+            
+        }
+    }
+</script>
+
+<style scoped>
+    h1{
+        color: blue;
+        font-size: 4em;
+        text-align: center;
+        font-weight: bold;
+    }
+</style>
